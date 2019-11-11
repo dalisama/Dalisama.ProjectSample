1a0a8d064655bce2b0eca855765d6738b424943b
Mohammed ali kachbouri <med.ali.kachbouri@hotmail.com>
2019-11-11 18:00:29 +0100

    Add project files.

---
+++ Dalisama.ProjectSimple.Gateway/Controllers/HealthController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dalisama.ProjectSimple.Gateway.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        [ActionName("Health")]
+        public ActionResult<string> Get()
+        {
+            return Ok("im ok");
+        }
+        [HttpGet]
+        [ActionName("BadHealth")]
+        public ActionResult<string> GetException()
+        {
+            throw new Exception();
+        }
+
+
+
+    }
+}
---
+++ Dalisama.ProjectSimple.Host.Common/Middleware/TraceLoggingMiddleware.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dalisama.ProjectSimple.Host.Common.Middleware
+{
+    public class TraceLoggingMiddleware
+    {
+
+
+        private readonly RequestDelegate _next;
+        private readonly ILogger<TraceLoggingMiddleware> _logger;
+
+        public TraceLoggingMiddleware(RequestDelegate next, ILogger<TraceLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //check if the request has already a correlation id
+            context.Request.Headers.TryGetValue("X-Request-Trace", out StringValues traceId);
+            // if no correlation id is found, generate a new one
+            if (traceId == default(StringValues)) traceId = Guid.NewGuid().ToString();
+            // add the correlation id to the response
+            context.Response.Headers.Add("X-Request-Trace", traceId);
+            // Adding the correlation id to all log related to the current request
+            using (_logger.BeginScope(new Dictionary<string, object> { ["X-Request-Trace"] = traceId.ToString() }))
+            {
+
+                try
+                {
+                    await _next(context);
+                }
+                catch (Exception ex)
+                {
+
+                    await HandleExceptionAsync(context, traceId, ex);
+                }
+            }
+        }
+        private Task HandleExceptionAsync(HttpContext context, string traceId, Exception exception)
+        {
+
+            var error = new
+            {
+                Id = traceId.ToString(),
+                Status = (short)HttpStatusCode.InternalServerError,
+                Title = "Some kind of error occurred in the API.  Please use the id and contact our " +
+                        "support team if the problem persists."
+            };
+
+
+
+            var innerExMessage = GetInnermostExceptionMessage(exception);
+
+            _logger.LogError(exception, innerExMessage + " -- {ErrorId}.", error.Id);
+
+            var result = JsonConvert.SerializeObject(error);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return context.Response.WriteAsync(result);
+        }
+
+        private string GetInnermostExceptionMessage(Exception exception)
+        {
+            if (exception.InnerException != null)
+                return GetInnermostExceptionMessage(exception.InnerException);
+
+            return exception.Message;
+        }
+    }
+}
+
